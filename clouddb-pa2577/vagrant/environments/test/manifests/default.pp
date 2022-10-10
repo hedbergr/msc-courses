dba9fb6d197c6e9b0bce5300a6252a2a27cc41d1
hedbergr <rasmus.d.hedberg@gmail.com>
2022-10-10 15:50:17 +0200

    Multi-machine deployment

--- clouddb-pa2577/vagrant/environments/test/manifests/default.pp
+++ clouddb-pa2577/vagrant/environments/test/manifests/default.pp
@@ -1,29 +1,15 @@
-
-
-exec { 'apt-update':
-    command => '/usr/bin/apt-get update'
+node 'appserver' {
+    include baseconfig, nodejs
 }
 
-Exec["apt-update"] -> Package <| |>
-
-package { "curl":
-    ensure  => latest
+node 'dbserver' {
+    include baseconfig, mysql
 }
 
-exec {"prepare nodejs":
-    command => 'curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -',
-    provider => shell,
-    onlyif => "/bin/bash -c 'command -v curl'"
+node 'web'  {
+    include baseconfig, nginx
 }
 
-package { "nodejs":
-    ensure => "latest"
-}
-
-package { "git":
-    ensure => "latest"
-}
-
-package { "build-essential":
-    ensure => "latest"
+node default {
+    include baseconfig
 }
